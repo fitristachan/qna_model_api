6fa7b431c845156984edafc4791e1b49593d665c
fitristachan <120694803+fitristachan@users.noreply.github.com>
2024-07-21 21:13:04 +0700

    add question param

--- deploy/app.py
+++ deploy/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from qna_pipeline import ask_question 
 import logging
 
@@ -17,7 +17,13 @@ def index():
     return jsonify({"status": "SUCCESS"}), 204
 
 @app.route('/qna/<int:context_id>', methods=['GET'])
-def get_answer(context_id: int):
+def get_answer(context_id):
+    question = request.args.get('question', type=str)
+
+    if question is None:
+        logging.error("Missing question in request")
+        return jsonify({'error': 'Missing question in request'}), 400
+
     logging.debug(f"Received context ID: {context_id}")
     context = next((ctx for ctx in contexts if ctx['id'] == context_id), None)
 
@@ -25,7 +31,7 @@ def get_answer(context_id: int):
         logging.error(f"Invalid context ID: {context_id}")
         return jsonify({'error': 'Invalid context ID'}), 400
 
-    return jsonify(ask_question(question="Apa sebutan Indonesia?", context=context['context']))
+    return jsonify(ask_question(question=question, context=context['context']))
 
 if __name__ == "__main__":
     app.run(debug=True)
