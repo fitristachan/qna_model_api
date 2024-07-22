2712eff586c8460bd651e5c2961e7bd5f5126eae
fitristachan <120694803+fitristachan@users.noreply.github.com>
2024-07-22 13:00:29 +0700

    migrate

--- deploy/app.py
+++ deploy/app.py
@@ -1,12 +1,37 @@
 from flask import Flask, request, jsonify
-from qna_pipeline import ask_question 
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
 import logging
 
+
+# CONTEXTS
 contexts = [
   {'id': 1, 'context': 'Indonesia dikenal sebagai negara gemah ripah loh jinawi. Sebutan ini muncul karena indonesia negara yang kaya.'},
   {'id': 2, 'context': 'Context 2'},
 ]
 
+
+# LOAD MODEL USING TRANSFORMERS LIBRARY
+def load_qna_model():
+    try:
+        tokenizer = AutoTokenizer.from_pretrained("indolem/indobert-base-uncased")
+        model = AutoModelForQuestionAnswering.from_pretrained("indolem/indobert-base-uncased")
+        pipe = pipeline("question-answering", model=model, tokenizer=tokenizer)
+        return tokenizer, model, pipe
+    except (ImportError, OSError) as e:
+        logging.error(f"Error loading QnA model: {e}")
+        return None, None, None
+
+def ask_question(question: str, context: str):
+    tokenizer, model, pipe = load_qna_model()
+    if not tokenizer or not model or not pipe:
+        return {"error": "No QnA model found"}
+
+    result = pipe(question=question, context=context)
+    return result
+
+
+
+# FLASK API
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG) 
