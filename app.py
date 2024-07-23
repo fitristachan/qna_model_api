51896003b1315a3ce0c3fcdc99d94701945e0b42
fitristachan <120694803+fitristachan@users.noreply.github.com>
2024-07-23 19:22:04 +0700

    change context as query arg

--- app.py
+++ app.py
@@ -2,13 +2,6 @@ from flask import Flask, request, jsonify
 from transformers import AutoTokenizer, AutoModelForQuestionAnswering, pipeline
 # import logging
 
-
-contexts = [
-  {'id': 1, 'context': 'Indonesia dikenal sebagai negara gemah ripah loh jinawi. Sebutan ini muncul karena indonesia negara yang kaya.'},
-  {'id': 2, 'context': 'Context 2'},
-]
-
-
 # LOAD MODEL USING TRANSFORMERS LIBRARY
 def load_qna_model():
     try:
@@ -40,22 +33,20 @@ def index():
     """Liveness check for the service"""
     return jsonify({"status": "Kelompok 2"}), 204
 
-@app.route('/qna/<int:context_id>', methods=['GET'])
-def get_answer(context_id):
+@app.route('/qna', methods=['GET'])
+def get_answer():
     question = request.args.get('question', type=str)
+    context = request.args.get('context', type=str)
 
     if question is None:
         # logging.error("Missing question in request")
         return jsonify({'error': 'Missing question in request'}), 400
 
-    # logging.debug(f"Received context ID: {context_id}")
-    context = next((ctx for ctx in contexts if ctx['id'] == context_id), None)
+    if context is None:
+        # logging.error("Missing context in request")
+        return jsonify({'error': 'Missing context in request'}), 400
 
-    if not context:
-        # logging.error(f"Invalid context ID: {context_id}")
-        return jsonify({'error': 'Invalid context ID'}), 400
-
-    return jsonify(ask_question(question=question, context=context['context']))
+    return jsonify(ask_question(question=question, context=context))
 
 if __name__ == "__main__":
     app.run(debug=True)
